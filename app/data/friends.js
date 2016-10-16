eed3590e13794de75e0fb0907d346b474223e558
Ryan Osterman <ryan_osterman@med.unc.edu>
2016-10-16 18:42:19 -0400

    added friend seed data

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		"name": "Richard",
+		"photo": "http://cartermatt.bgmyzuarasgpknxgxbrs.netdna-cdn.com/wp-content/uploads/2015/06/Richard-Hendricks.jpg?x94793",
+		"scores": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+	},
+	{
+		"name": "Bachman",
+		"photo": "http://www.ballermindframe.com/wp-content/uploads/2016/06/silicon-valley-bachman.jpg",
+		"scores": [5, 4, 3, 2, 1, 5, 4, 3, 2, 1]
+	},
+	{
+		"name": "Gilfoyle",
+		"photo": "http://i.imgur.com/0cpbtC8.jpg",
+		"scores": [1, 3, 5, 3, 1, 3, 5, 3, 1, 3]
+	},
+	{
+		"name": "Dinesh",
+		"photo": "http://filmgarb.com/wp-content/uploads/tv-silicon_valley-2014_-dinesh-kumail_nanjiani-shirts-s01e03-le_tigre_rugby_shirts.jpg",
+		"scores": [2, 4, 2, 4, 2, 4, 2, 4, 2, 4]
+	},
+	{
+		"name": "Jared",
+		"photo": "http://pixel.nymag.com/imgs/daily/vulture/2015/05/15/recaps/15-silicon-valley.w1200.h630.jpg",
+		"scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	}
+];
+
+module.exports = friends;
