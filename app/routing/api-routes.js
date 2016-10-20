a51457d9d63c81feeaaccd82239ee2d0df4dabfb
Ryan Osterman <ryan_osterman@med.unc.edu>
2016-10-19 21:01:13 -0400

    mostly working

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,5 +1,5 @@
 var friends = require('../data/friends.js');
-var path = require('path');
+// var path = require('path');
 
 module.exports = function(app){
 
@@ -11,8 +11,30 @@ module.exports = function(app){
 
 	app.post('/api/friends', function(req, res){
 
+		for(var i = 0; i < req.body.scores.length; i++){
+			var ints = parseInt(req.body.scores[i]);
+			req.body.scores[i] = ints;
+		}
+
+		friends.push(req.body);
+
+		var mostCompatible = 0;
+		var lowestDif = 40;
+
+		for(var i = 0; i < friends.length - 1; i++){
+			var totalDif = 0;
 		
+			for(var j = 0; j < friends[i].scores.length; j++){
+				totalDif += Math.abs(friends[i].scores[j] - req.body.scores[j]);
+			}
 
-	})
+			if(totalDif < lowestDif){
+				mostCompatible = i;
+				lowestDif = totalDif;
+			}
+		}
 
-}
+		res.send(friends[mostCompatible]);
+	});
+
+};
--- server.js
+++ server.js
@@ -10,8 +10,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-app.use(express.static('app/public'));
-
 require('./app/routing/api-routes.js')(app); 
 require('./app/routing/html-routes.js')(app);
 
